a681a6e6ce5a900b4982bfbb523d9abff0396d37
cherry2501 <saicharangudiya@gmail.com>
2025-01-01 20:35:56 +0530

    terraform

---
+++ Ec2/ec2.tf
@@ -0,0 +1,36 @@
+resource "aws_security_group" "allow-ssh" {
+    
+    name = "allow-ssh"
+    description = "Allow port no 22 for ssh traffic"
+
+    egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+  
+}
+
+resource "aws_instance" "Terraform" {
+    ami = "ami-09c813fb71547fc4f"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.allow-ssh.id]
+    tags = {
+    Name = "terraform"
+  }
+  
+}
