33ff2d31fa2f61bf39a5cfe9e456e4cbb053bbbd
cherry2501 <saicharangudiya@gmail.com>
2025-01-19 20:16:51 +0530

    terraform

--- Ec2/ec2.tf
+++ Ec2/ec2.tf
@@ -15,7 +15,7 @@ resource "aws_security_group" "allow-ssh" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    cidr_blocks      = ["0.0.0.0/0"] #allow everyone
     ipv6_cidr_blocks = ["::/0"]
   }
 
