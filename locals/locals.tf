2e61eb125cc9de25abf3542725aeca1d42c03bd8
cherry2501 <saicharangudiya@gmail.com>
2025-01-20 01:00:51 +0530

    terraform

--- locals/locals.tf
+++ locals/locals.tf
@@ -2,9 +2,9 @@ locals {
   domain_name = "charan.online"
   zone_id = "Z01267773J57SG8MZVSO"
   instance_type = var.environment == "prod" ? "t3.medium" : "t3.micro"
-  
+
 
 }
 
 #count.index won't work in locals
-# values can't be overwrite
+# values can't be override
